94029b57c244d23a31828a9d1219fa9236bda451
Roger Medrano <email@example.com>
2021-09-27 09:00:44 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string makeOfFirstCar = "GMC";
+            int yearOfCar = 2011;
+            char firstLetterOfModel = 'A';
+            bool lease = true;
+            double carPayment = 150.00;
+            decimal engineSize = 4.7M;
+
+            Console.WriteLine($"My first car was a {yearOfCar} {makeOfFirstCar}. I paid {carPayment} a friggin month for it.");
+
+        }
+    }
+}
